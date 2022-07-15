80f0c98770da7cad0c32ee78cab760fdb528d25b
Irvan Saharudin <irvansaharudin@gmail.com>
2022-07-15 14:25:41 +0700

    gamecard v 4.0.0

--- components/GameCard/index.jsx
+++ components/GameCard/index.jsx
@@ -1,6 +1,8 @@
 import style from './GameCard.module.css';
 import Star from "../Star";
-import Link from 'next/link'
+import Link from 'next/link';
+import Image from 'next/image';
+import { toast } from 'react-toastify';
 import {BsBookmark, BsFillBookmarkFill, BsPlay, BsPlayFill} from 'react-icons/bs'
 import {FaPlaystation, FaXbox, FaAndroid} from 'react-icons/fa'
 import {SiNintendoswitch} from 'react-icons/si';
@@ -13,21 +15,54 @@ export default function GameCard({content = {}}){
     const [ bookmark, setBookmark] = useState(false);
 
     function handlePlay(){
-        setPlayed( curr => curr = !curr)
+        setPlayed( curr => curr = !curr);
+        showToast(played, 'play list');
     }
 
     function handleBookmark(){
-        setBookmark( curr => curr = !curr)
+        setBookmark( curr => curr = !curr);
+        showToast(bookmark, 'bookmark list');
     }
+
+    function showToast(par, msg){
+        if(!par){
+          toast.success(`added to ${msg}`, {
+            position: "top-center",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: 'dark'
+            });
+        }else{
+          toast.info(`deleted from ${msg}`, {
+            position: "top-center",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: 'dark'
+            });
+        }
+      }
 
     return(
         <div className={style.container}>
-            {/* game image */}
+            {/* game image carousel */}
             <Link href={`/Games/${content.slug}`}>
                 <GameCardCarousel img={content.short_screenshots ? content.short_screenshots : [background_image]} />
             </Link>
+
             {/* game content */}
             <div className={style.contentContainer}>
+                <div className={style.gameProfilePic}>
+                    <Image src={content.background_image} layout='fill'></Image>
+                </div>
+
                 <div className={style.content}>
                     {/* game header */}
                     <div className={style.header}>
@@ -36,25 +71,12 @@ export default function GameCard({content = {}}){
                                 {content.name}
                             </Link>
                         </h3>
-                        {/* game feature */}
-                        <div className={style.featureContainer}>
-                            <div 
-                            className={style.feature}
-                            onClick ={() => handlePlay()}>
-                                {played ? <BsPlayFill /> : <BsPlay />}
-                            </div>
-                            <div 
-                            className={style.feature}
-                            onClick={()=> handleBookmark()}>
-                                {bookmark ? <BsFillBookmarkFill /> : <BsBookmark />}
-                            </div>
-                        </div>
                     </div>
                      <div className={style.starContainer} title={`${content.rating}`}>
-                            <Star limit={content.rating} />
+                             <Star limit={content.rating} />
                     </div>
                     {/* additional information about games */}
-                    <div className={style.metaContainer}>
+                    <div className={style.body}>
                         <div 
                             className={style.meta} 
                             title='metacritic score'
@@ -64,40 +86,53 @@ export default function GameCard({content = {}}){
                             }}>
                             {content.metacritic}
                         </div>
-                    </div>
-                    <div className={style.genres}>
-                        <div className={style.linkContainer}>
-                        { content.genres.map(genre =>{
-                            return(
-                                <p className={style.link} key={genre.id} title={`${genre.games_count} games`}>
-                                    <span>genre :</span>
-                                    <Link href={`/Genres/${genre.slug}`}>
-                                        {genre.name}
-                                    </Link>
-                                </p>
-                            )
+                        <div className={style.genres}>
+                            <div className={style.linkContainer}>
+                            { content.genres.map(genre =>{
+                                return(
+                                        <Link href={`/Genres/${genre.slug}`}>
+                                            {`${genre.name}, `}
+                                        </Link>
+                                    )
+                                })}
+                            </div>
+                        </div>
+                            <div className={`${style.platformsContainer} ${style.linkContainer}`}>
+                            {content.parent_platforms.map(content =>{
+                                return(
+                                    <span key={content.platform.id} title={content.platform.name}>
+                                        <Link href={`/platforms/${content.platform.slug}`}>
+                                        {
+                                            content.platform.slug.includes('playstation') ? <FaPlaystation /> :
+                                            content.platform.slug.includes('xbox') ? <FaXbox /> :
+                                            content.platform.slug.includes('nintendo') ? <SiNintendoswitch /> :
+                                            content.platform.slug.includes('android') ? <FaAndroid /> :
+                                            content.platform.slug.includes('pc') ? <AiOutlineDesktop /> :
+                                            content.platform.slug.includes('ios') ? <AiFillApple /> :
+                                            null
+                                        }
+                                        </Link>
+                                    </span>
+                                )
                             })}
                         </div>
                     </div>
-                    <div className={`${style.platformsContainer} ${style.linkContainer}`}>
-                       {content.parent_platforms.map(content =>{
-                        return(
-                            <span key={content.platform.id} title={content.platform.name}>
-                                <Link href={`/platforms/${content.platform.slug}`}>
-                                {
-                                    content.platform.slug.includes('playstation') ? <FaPlaystation /> :
-                                    content.platform.slug.includes('xbox') ? <FaXbox /> :
-                                    content.platform.slug.includes('nintendo') ? <SiNintendoswitch /> :
-                                    content.platform.slug.includes('android') ? <FaAndroid /> :
-                                    content.platform.slug.includes('pc') ? <AiOutlineDesktop /> :
-                                    content.platform.slug.includes('ios') ? <AiFillApple /> :
-                                    null
-                                }
-                                </Link>
-                            </span>
-                        )
-                       })}
+                </div>
+
+                     {/* game feature */}
+                <div className={style.wrapper}>
+                <div className={style.featureContainer}>
+                    <div 
+                        className={style.feature}
+                        onClick ={() => handlePlay()}>
+                            {played ? <BsPlayFill /> : <BsPlay />}
                     </div>
+                    <div 
+                        className={style.feature}
+                        onClick={()=> handleBookmark()}>
+                            {bookmark ? <BsFillBookmarkFill /> : <BsBookmark />}
+                    </div>
+                </div>
                 </div>
             </div>
         </div>
--- components/GameCardCarousel/index.jsx
+++ components/GameCardCarousel/index.jsx
@@ -3,16 +3,15 @@ import style from './GameCardCarousel.module.css';
 import Image from 'next/image';
 
 export default function GameCardCarousel({img = []}) {
-    const [shownImg, setShownImg] = useState(0);
-    const [mousePos, setMousePos] = useState(0);
+    const [shownImg, setShownImg] = useState(1);
+    const [mousePos, setMousePos] = useState(1);
 
 
     function handleMouse(e){
-        console.log();
         setMousePos(curr => curr = e.nativeEvent.offsetX);
         const total = mousePos / (e.target.clientWidth / img.length);
         const imgPos = parseInt(total.toFixed());
-        imgPos >= img.length ? setShownImg(img.length - 1) : setShownImg(curr => curr = imgPos);
+        imgPos === 0 ? setShownImg(1) : imgPos >= img.length ? setShownImg(img.length - 1) : setShownImg(curr => curr = imgPos);
     }
 
   return (
@@ -21,6 +20,7 @@ export default function GameCardCarousel({img = []}) {
             className={style.carousel}
             onMouseMove={(e) => handleMouse(e)}>
             {img.map((item, index) =>{
+                if(item.id != '-1'){
                 return(
                     <div 
                     key={item.id? item.id : index}
@@ -31,19 +31,22 @@ export default function GameCardCarousel({img = []}) {
                         <Image src={item.image} layout='fill' priority='true'/>
                     </div>
                 )
+                }
             })}
         </div>
         <div className={style.indicatorContainer}>
             {img.map((item, index)=>{
-                return(
-                    <div 
-                        key={item.id ? item.id : index} 
-                        className={style.indicator}
-                        style={{
-                           flexBasis: `${1 / img.length * 100}%`,
-                           backgroundColor: `${index === shownImg ? 'var(--text-col)' : 'var(--blur-bg-col)'}`
-                        }}></div>
-                )
+                if(item.id != '-1'){
+                    return(
+                        <div 
+                            key={item.id ? item.id : index} 
+                            className={style.indicator}
+                            style={{
+                               flexBasis: `${1 / img.length * 100}%`,
+                               backgroundColor: `${index === shownImg ? 'var(--text-col)' : 'var(--blur-bg-col)'}`
+                            }}></div>
+                    )
+                }
             })}
         </div>
     </div>
