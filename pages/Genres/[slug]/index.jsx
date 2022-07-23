f68bdd14dba56ea14e8f0897420218d256ca8966
Irvan Saharudin <irvansaharudin@gmail.com>
2022-07-23 13:15:54 +0700

    create genre pages starter files

---
+++ pages/Genres/[slug]/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Components from '../../../components'
+
+export default function GenrePages() {
+    const {NestedLayout} = Components();
+  return (
+    <NestedLayout>
+        <div>GenrePages</div>
+    </NestedLayout>
+  )
+}
